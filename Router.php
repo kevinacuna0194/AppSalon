a422c856902aaf6ee2bf44d7e7133cedcb7d297c
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2023-02-17 15:49:35 -0300

    Creando el Routing y Controlador para Autenticación

--- Router.php
+++ Router.php
@@ -19,15 +19,6 @@ class Router
 
     public function comprobarRutas()
     {
-
-        // Proteger Rutas...
-        session_start();
-
-        // Arreglo de rutas protegidas...
-        // $rutas_protegidas = ['/admin', '/propiedades/crear', '/propiedades/actualizar', '/propiedades/eliminar', '/vendedores/crear', '/vendedores/actualizar', '/vendedores/eliminar'];
-
-        // $auth = $_SESSION['login'] ?? null;
-
         $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
         $method = $_SERVER['REQUEST_METHOD'];
 
@@ -36,7 +27,6 @@ class Router
         } else {
             $fn = $this->postRoutes[$currentUrl] ?? null;
         }
-
 
         if ($fn) {
             // Call user fn va a llamar una función cuando no sabemos cual sera
@@ -48,7 +38,6 @@ class Router
 
     public function render($view, $datos = [])
     {
-
         // Leer lo que le pasamos  a la vista
         foreach ($datos as $key => $value) {
             $$key = $value;  // Doble signo de dolar significa: variable variable, básicamente nuestra variable sigue siendo la original, pero al asignarla a otra no la reescribe, mantiene su valor, de esta forma el nombre de la variable se asigna dinamicamente
--- gulpfile.js
+++ gulpfile.js
@@ -42,14 +42,14 @@ function imagenes() {
     return src(paths.imagenes)
         .pipe(cache(imagemin({ optimizationLevel: 3 })))
         .pipe(dest('public/build/img'))
-    // .pipe(notify({ message: 'Imagen Completada'}));
+        .pipe(notify({ message: 'Imagen Completada' }));
 }
 
 function versionWebp() {
     return src(paths.imagenes)
         .pipe(webp())
         .pipe(dest('public/build/img'))
-    // .pipe(notify({ message: 'Imagen Completada'}));
+        .pipe(notify({ message: 'Imagen Completada' }));
 }
 
 function watchArchivos() {
