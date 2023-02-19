d637224d7627e4b2a6d5c8921427b0acb47f7b13
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2023-02-19 14:54:07 -0300

    Creando el Modelo de Usuarios

--- views/layout.php
+++ views/layout.php
@@ -7,6 +7,7 @@
     <title>App Salón</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="build/css/app.css">
+    <link rel="icon" href="build/img/salon.ico" type="image/x-icon">
 </head>
 
 <body>
