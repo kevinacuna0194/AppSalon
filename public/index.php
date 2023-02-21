12ec9115d6c4b593c829ec0578882731305556b0
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2023-02-21 13:18:21 -0300

    Creando Routing y Controllers para Citas

--- public/index.php
+++ public/index.php
@@ -4,15 +4,16 @@ require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
 use Controllers\LoginController;
+use Controllers\CitaController;
 
 $router = new Router();
 
-/** Inisiar Sesión */
+/** Iniciar Sesión */
 $router->get('/', [LoginController::class, 'login']);
 $router->post('/', [LoginController::class, 'login']);
 $router->get('/logout', [LoginController::class, 'logout']);
 
-/** Recuperar Passwor */
+/** Recuperar Password */
 $router->get('/olvide', [LoginController::class, 'olvide']);
 $router->post('/olvide', [LoginController::class, 'olvide']);
 $router->get('/recuperar', [LoginController::class, 'recuperar']);
@@ -25,6 +26,9 @@ $router->post('/crear-cuenta', [LoginController::class, 'crear']);
 /** Confirmar cuenta */
 $router->get('/confirmar-cuenta', [LoginController::class, 'confirmar']);
 $router->get('/mensaje', [LoginController::class, 'mensaje']);
+
+/** Área privada */
+$router->get('/cita', [CitaController::class, 'index']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
