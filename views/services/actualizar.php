0dbab58dc2ec7494f19befbb0c1d80ba6ee9e4a3
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2023-03-06 16:56:33 -0300

    Creando el Formulario para Servicios

---
+++ views/services/actualizar.php
@@ -0,0 +1,5 @@
+<h1 class="nombre-pagina">Actualizar Servicio</h1>
+
+<p class="descripcion-pagina">Modifica los Valores del Formulario</p>
+
+<?php include_once __DIR__ . '/../templates/barra.php'; ?>
