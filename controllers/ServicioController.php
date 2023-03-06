87d8d5e020b05876624d3e0295da8b0fa9943776
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2023-03-06 16:45:59 -0300

    Creando las Vistas para los Servicios

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -8,27 +8,37 @@ class ServicioController
 {
     public static function index(Router $router)
     {
-        $router->render('services/index', []);
+        session_start();
+
+        $router->render('services/index', [
+            'nombre' => $_SESSION['nombre']
+        ]);
     }
 
     public static function crear(Router $router)
     {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        session_start();
 
-        }
+        $router->render('services/crear', [
+            'nombre' => $_SESSION['nombre']
+        ]);
     }
 
     public static function actualizar(Router $router)
     {
+        session_start();
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            
         }
+
+        $router->render('services/actualizar', [
+            'nombre' => $_SESSION['nombre']
+        ]);
     }
 
     public static function eliminar(Router $router)
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            
         }
     }
 }
