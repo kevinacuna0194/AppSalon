15be6579f51f405263a2c095c167ad8676b216ad
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2023-03-06 20:00:41 -0300

    Crear Variables de Entorno

--- includes/app.php
+++ includes/app.php
@@ -1,8 +1,12 @@
 <?php 
 
+require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+/** Si ese archivo no existe no va a marcar un error */
+$dotenv->safeLoad();
+
 require 'funciones.php';
 require 'database.php';
-require __DIR__ . '/../vendor/autoload.php';
 
 // Conectarnos a la base de datos
 use Model\ActiveRecord;
